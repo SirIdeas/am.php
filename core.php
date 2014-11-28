a570e7276cb0cf13dea2c310158e767ca5afec9f
Alex J. Rondón <alex@sirideas.com>
2014-11-28 11:43:50 -0430

    Nucleo

---
+++ core.php
@@ -0,0 +1,14 @@
+<?php
+
+// Si la constante AM_FOLDER está definida entonces generar error
+!defined("AM_FOLDER") or die("Am: Alreadys defined \"AM_FOLDER\"");
+
+// Definir constante con la carpeta contenedora framework
+define("AM_FOLDER", dirname(__FILE__) . "/");
+
+// Incluir núcleo de framework
+require AM_FOLDER . "/core/Am.php";
+require AM_FOLDER . "/core/AmObject.php";
+
+// Realizar llamada
+Am::task();
