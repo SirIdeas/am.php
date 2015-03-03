8c97562adea7c24f2b97d52969414429fcbd164d
unknown <arondn2@gmail.com>
2015-03-03 09:37:16 -0430

    Fix problem detect request query from REQUEST_URL instead of REDIRECT_URI.

--- core/Am.class.php
+++ core/Am.class.php
@@ -488,8 +488,14 @@ final class Am{
         self::$urlBase = "";
       
       // Obtener peticion
-      // $request = substr_replace($_SERVER["REQUEST_URI"], "", 0, strlen(self::$urlBase));
-      $request = substr_replace($_SERVER["REDIRECT_URL"], "", 0, strlen(self::$urlBase));
+      // $request = substr_replace($_SERVER["REDIRECT_URL"], "", 0, strlen(self::$urlBase));
+      $request = substr_replace($_SERVER["REQUEST_URI"], "", 0, strlen(self::$urlBase));
+
+      // Quitar los parametros
+      if(!empty($_SERVER["QUERY_STRING"]))
+        $request = substr_replace($request, "",
+          strlen($request) - strlen($_SERVER["QUERY_STRING"]) - 1,
+          strlen($_SERVER["QUERY_STRING"]) + 1);
 
       // Validacion para cuando este en la peticion no comienze con "/"
       if(empty($request) || $request[0] !== "/")
