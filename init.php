95b7a0715a1f975f0f9d8545798007d37143aeb9
Alexander Rondon <alex@sirideas.com>
2015-06-20 18:34:33 -0430

    replace double quotes for simple quotes where is not necessary double qoutes

--- init.php
+++ init.php
@@ -24,12 +24,12 @@
  **/
 
 // Version de Amathista
-define("AM_VERSION", "0.1.0-beta");
+define('AM_VERSION', '0.1.0-beta');
 
 // Incluir núcleo de framework
-// require dirname(__FILE__) . "/core/errors.php";
-require dirname(__FILE__) . "/core/helpers.php";
-require dirname(__FILE__) . "/core/Am.class.php";
-require dirname(__FILE__) . "/core/AmObject.class.php";
+// require dirname(__FILE__) . '/core/errors.php';
+require dirname(__FILE__) . '/core/helpers.php';
+require dirname(__FILE__) . '/core/Am.class.php';
+require dirname(__FILE__) . '/core/AmObject.class.php';
 
-Am::addDir(dirname(__FILE__) . "/"); // Agregar ruta del nucle de amathista
+Am::addDir(dirname(__FILE__) . '/'); // Agregar ruta del nucle de amathista
