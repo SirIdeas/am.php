180cdfdd11a5db107488b75f4cf859fc5029bdec
Alex J. Rondon <alex@sirideas.com>
2015-12-20 02:04:11 -0430

    Fix Am::mimteType

---
+++ am/controllers.conf.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Amathista - PHP Framework
+ *
+ * @author Alex J. Rondón <arondn2@gmail.com>
+ * 
+ */
+/**
+ * -----------------------------------------------------------------------------
+ * Configuración de los controladores
+ * -----------------------------------------------------------------------------
+ */
+return array(/**/);
--- am/core/Am.class.php
+++ am/core/Am.class.php
@@ -1428,8 +1428,8 @@ final class Am{
 
     $mimePath = self::findFile('rsc/mime.types');
     $fileext = substr(strrchr($filename, '.'), 1);
-    if (empty($fileext)) return (false);
-    $regex = '/^([\w\+\-\.\/]+)\s+(\w+\s)*($fileext\s)/i';
+    if (empty($fileext)) return false;
+    $regex = "/^([\w\+\-\.\/]+)\s+(\w+\s)*($fileext\s)/i";
     $lines = file($mimePath);
     foreach($lines as $line) {
       if (substr($line, 0, 1) == '#') continue; // skip comments
@@ -1437,7 +1437,7 @@ final class Am{
       if (!preg_match($regex, $line, $matches)) continue; // no match to the extension
       return ($matches[1]);
     }
-    return (false); // no match at all
+    return false; // no match at all
 
   }
 
