e70626f0fb9ca2b018e3d35633a8fe5b15ba4214
Alex J. Rondon <alex@sirideas.com>
2016-01-30 00:45:30 -0430

    Side bar added API links. Added models page

--- app/views/sidebar.php
+++ app/views/sidebar.php
@@ -3,15 +3,12 @@
     <div class="col s3 side-bar-inner">
       
       <div class="title-parent">
-        <img src="(:/:)/images/am_vlogo_p.png" alt="" class="sub-header-logo">
+        <a href="(:/:)">
+          <img src="(:/:)/images/am_vlogo_p.png" alt="" class="sub-header-logo">
+        </a>
       </div>
 
       <ul class="nav spyscroll" data-height="#pageTitle" data-str="float">
-        <li>
-          <a href="(:/:)">Inicio</a>
-          <ul class="sub-nav">
-          </ul>
-        </li>
         <li><span>Primeros pasos</span>
           <ul class="sub-nav">
             <li><a href="(:/:)/introduction">Introducción</a></li>
@@ -19,6 +16,7 @@
             <li><a href="(:/:)/routing">Rutas</a></li>
             <li><a href="(:/:)/views">Vistas</a></li>
             <li><a href="(:/:)/controllers">Controladores</a></li>
+            <li><a href="(:/:)/models">Modelos</a></li>
           </ul>
         </li>
         <li>
@@ -26,6 +24,47 @@
           <ul class="sub-nav">
             <li><a href="(:/:)/configure-server#apache">Apache</a></li>
             <li><a href="(:/:)/configure-server#nginx">Nginx</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="(:/:)/api">API</a>
+          <ul class="sub-nav">
+            <li><span><strong><a href="(:/:)/api/core/">core</a></strong></span>
+              <ul class="sub-nav">
+                <li><a href="(:/:)/api/core#Am">Am</a></li>
+                <li><a href="(:/:)/api/core#AmObject">AmObject</a></li>
+                <li><a href="(:/:)/api/core#AmError">AmError</a></li>
+              </ul>
+            </li>
+            <li><span><strong><a href="(:/:)/api/AmRoute">AmRoute</a></strong></span>
+              <ul class="sub-nav">
+                <li><a href="(:/:)/api/AmRoute#AmRoute">AmRoute</a></li>
+              </ul>
+            </li>
+            <li><span><strong><a href="(:/:)/api/AmTpl">AmTpl</a></strong></span>
+              <ul class="sub-nav">
+                <li><a href="(:/:)/api/AmTpl#AmTpl">AmTpl</a></li>
+              </ul>
+            </li>
+            <li><span><strong><a href="(:/:)/api/AmResponse">AmResponse</a></strong></span>
+              <ul class="sub-nav">
+                <li><a href="(:/:)/api/AmResponse#AmResponse">AmResponse</a></li>
+                <li><a href="(:/:)/api/AmResponse#AmCallResponse">AmCallResponse</a></li>
+                <li><a href="(:/:)/api/AmResponse#AmFileResponse">AmFileResponse</a></li>
+                <li><a href="(:/:)/api/AmResponse#AmRedirectResponse">AmRedirectResponse</a></li>
+                <li><a href="(:/:)/api/AmResponse#AmTemplateResponse">AmTemplateResponse</a></li>
+              </ul>
+            </li>
+            <li><span><strong><a href="(:/:)/api/AmController">AmController</a></strong></span>
+              <ul class="sub-nav">
+                <li><a href="(:/:)/api/AmController#AmController">AmController</a></li>
+              </ul>
+            </li>
+            <li><span><strong><a href="(:/:)/api/AmCoder">AmCoder</a></strong></span>
+              <ul class="sub-nav">
+                <li><a href="(:/:)/api/AmCoder#AmCoder">AmCoder</a></li>
+              </ul>
+            </li>
           </ul>
         </li>
       </ul>
