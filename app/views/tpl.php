a48ad5dbcca516e98b88428795f1a2874965ecf1
Alex J. Rondon <alex@sirideas.com>
2016-03-13 15:01:27 -0430

    updated tpl

--- app/views/tpl.php
+++ app/views/tpl.php
@@ -4,17 +4,26 @@
   <head>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="(:/:)/favicon.ico"/>
     <title>
       (:= $siteTitle :) |
       (:= $pageTitle :)
     </title>
-    <link rel="shortcut icon" href="(:/:)/favicon.ico"/>
     <link rel="stylesheet" href="(:/:)/css/fonts.css"/>
     <link rel="stylesheet" href="(:/:)/css/styles.css"/>
     <link rel="stylesheet" href="(:/:)/vendor/prism/prism.css"/>
+    
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link rel="stylesheet" href="(:/:)/fixes/ie10-viewport-bug-workaround.css">
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="(:/:)/fixes/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="(:/:)/fixes/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
   <body>
