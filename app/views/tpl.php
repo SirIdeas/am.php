b1b0f98bbf11ba80e60f74a7594d2fc423760b60
Alex J. Rondon <alex@sirideas.com>
2016-03-17 07:02:47 -0430

    change google analytics

--- app/views/tpl.php
+++ app/views/tpl.php
@@ -33,7 +33,8 @@
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-32728007-15', 'auto');ga('send', 'pageview');
+      ga('create', 'UA-41615550-14', 'auto');
+      ga('send', 'pageview');
     </script>
     <script type="text/javascript" src="(:/:)/vendor/jquery/jquery.js"></script>
     <script type="text/javascript" src="(:/:)/vendor/prism/prism.js"></script>
