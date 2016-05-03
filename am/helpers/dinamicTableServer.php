125be6410baec66497f6ca6edefbbf9c49c5c24a
Alex J. Rondon <alex@sirideas.com>
2016-05-03 11:52:00 -0400

    dinamicTableServer helper

---
+++ am/helpers/dinamicTableServer.php
@@ -0,0 +1,48 @@
+<?php
+
+function dinamicTableServer(AmObject $params, AmQuery $query, $toArray = true){
+
+  $fields = $query->getSelects();
+
+  if(empty($fields))
+    $fields = $query->getTable(true)->getFields();
+  
+  $fields = array_keys($fields);
+  $query = $query->getScheme()->q($query)->setSelects(array_combine($fields, $fields));
+  
+  $count = $query->count();
+  
+  if(isset($params->oSorts))
+    foreach($params->oSorts as $dir)
+      if(!empty($dir))
+        $query->orderBy(array($fields[$dir['pos']]), $dir['dir']);
+  
+  if(!empty($params->sSearch))
+    foreach($fields as $f)
+      $query->orWhere("{$f} LIKE '%{$params->sSearch}%'");
+  
+  if(is_array($params->oSearch))
+    foreach($params->oSearch as $pos => $val)
+      $query->andWhere("{$fields[$pos]} LIKE '%{$val}%'");
+  
+  $countResult = $query->count();
+  
+  if($params->iLen != -1)
+    $query
+      ->limit($params->iLen)
+      ->offSet($params->iPage * $params->iLen);
+
+  $records = $query->get('array', function($record) use ($fields){
+    $record = AmObject::mask($record, $fields);
+    if($toArray === true)
+      return array_values($record);
+    return $record;
+  });
+
+  return array(
+    'aoData' => $records,
+    'iRecordCount' => $count,
+    'iRecordFilteredCount' => $countResult
+  );
+    
+}
